1cddd79bf8a1c98e7e0b8cb98fcf8ffd41964878
Hening Wang <hening.wang0615@gmail.com>
2026-03-06 19:03:04 +0100

    norming study live ready

--- experiments/norming_exp/src/magpie.config.js
+++ experiments/norming_exp/src/magpie.config.js
@@ -1,11 +1,11 @@
 export default {
-  experimentId: '10',
-  serverUrl: 'https://magpie-demo.herokuapp.com/',
-  socketUrl: 'wss://magpie-demo.herokuapp.com/socket',
+  experimentId: '58',
+  serverUrl: 'https://magpie-cogsciprag.fly.dev',
+  socketUrl: 'wss://magpie-cogsciprag.fly.dev/socket',
   // this will be used in prolific mode
-  completionUrl: 'https://...',
-  contactEmail: 'test@random.com',
+  completionUrl: 'https://app.prolific.com/submissions/complete?cc=C1PFHBT7',
+  contactEmail: 'hening.wang@outlook.com',
   // Either 'debug', 'directLink' or 'prolific'
-  mode: 'debug',
+  mode: 'prolific',
   language: 'de'
 };
